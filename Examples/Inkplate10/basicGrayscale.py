568f700a13d86c0db5e98f34536d32eea2edf98d
TAVU - Robert <robertperic@outlook.com>
2021-05-20 08:11:35 +0200

    Grayscale fix in waveforms for 6 and 10
    Grayscale fix and grayscale example fix for 6 and 10.

--- Examples/Inkplate10/basicGrayscale.py
+++ Examples/Inkplate10/basicGrayscale.py
@@ -7,24 +7,26 @@ import time
 # Initialize inkplate display
 display = Inkplate(Inkplate.INKPLATE_2BIT)
 
-color = 1
 
 # Main function, you can make infinite while loop inside this to run code indefinitely
 if __name__ == "__main__":
     # Must be called before using, line in Arduino
     display.begin()
-    while True:
-        display.clearDisplay()
-        display.display()
+    display.clearDisplay()
+    display.display()
 
-        # All drawing functions
-        display.writeFillRect(0, 0, 50, 600, color)
+    # Draw palet of posible colors
+    #use color values 0, 1, 2, 3
+    display.writeFillRect(0, 0, 25, 600, 3)
+    display.writeFillRect(25, 0, 25, 600, 2)
+    display.writeFillRect(50, 0, 25, 600, 1)
+    display.writeFillRect(75, 0, 25, 600, 0)
 
-        display.display()
-        time.sleep(3)
+    display.display()
+    time.sleep(3)
 
-        # Draws image from bytearray
-        display.setRotation(0)
-        display.drawBitmap(120, 200, image, 576, 100)
-        display.display()
-        time.sleep(10)
+    # Draws image from bytearray
+    display.setRotation(0)
+    display.drawBitmap(120, 200, image, 576, 100)
+    display.display()
+    time.sleep(10)
