6853c0b3d801c182152387e43df3818d6c91de78
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2026-02-09 16:08:02 +0100

    Add Inkplate 13SPECTRA Micropython module

---
+++ Examples/Inkplate13SPECTRA/rtc.py
@@ -0,0 +1,30 @@
+# FILE: rtc.py
+# AUTHOR: Soldered
+# BRIEF: An example showing how to use the onboard RTC
+# LAST UPDATED: 2026-02-09
+
+# Include all the required libraries
+from inkplate13SPECTRA import Inkplate
+import time
+
+# Create Inkplate object
+inkplate = Inkplate()
+
+# Initialize the display, needs to be called only once
+inkplate.begin()
+
+# This is how to set the RTC's time
+# Arguments are hour, minute, seconds
+inkplate.rtcSetTime(9,39,10)
+# And this is the date
+# Arguments are weekday, day in month, month and year
+inkplate.rtcSetDate(2,9,2,2026)
+
+# Infinite loop
+while True:
+
+    # Show the set time
+    print(inkplate.rtcGetData())
+
+    # Let's wait 10 seconds
+    time.sleep(10)
